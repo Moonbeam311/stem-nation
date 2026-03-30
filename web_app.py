582f40a0b5859c56c6c25d2ba98d403d5da37a5b
Moonbeam311 <noirmoon311@gmail.com>
2026-03-30 12:08:17 -0400

    Fix Flask host and port binding for Render

--- web_app.py
+++ web_app.py
@@ -1,3 +1,4 @@
+import os
 
 from engines.engine_pipeline import apply_decision
 from flask import Flask, render_template, request
@@ -120,4 +121,7 @@ def hub():
     return render_template("project_hub.html")
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
     app.run(debug=True)
